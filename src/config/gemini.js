a2e1b19ca83bfa3fc42faed990bc6bf3186ae309
Pradesh Gwachha <pradesh@cognerahealth.ai>
2025-03-08 21:24:56 -0600

    gemini-clone completed

---
+++ src/config/gemini.js
@@ -0,0 +1,34 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+// const apiKey = process.env.AIzaSyCGHivlDK6G9NQIPkeBVS9LbsjE4jU9lAs;
+const apiKey = "AIzaSyCGHivlDK6G9NQIPkeBVS9LbsjE4jU9lAs";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
